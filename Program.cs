85461ecdba492370809ca47bb04540101e71b7dc
Erik-Aron-Szabo <54311015+Erik-Aron-Szabo@users.noreply.github.com>
2019-12-02 22:23:41 +0100

    Add files via upload
    Ver 1.0

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Waste_Recycle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
